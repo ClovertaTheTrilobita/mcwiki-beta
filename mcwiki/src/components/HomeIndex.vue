8c221349a6a1538cc7c9193a2190a09bbd0e9a35
ClovertaTheTrilobita <cloverta@petalmail.com>
2024-10-12 17:15:03 +0800

    projects combined

--- mcwiki/src/components/HomeIndex.vue
+++ mcwiki/src/components/HomeIndex.vue
@@ -1,13 +1,32 @@
-﻿<template>
-  <h1>home page</h1>
-  <p>1</p>
-</template>
+﻿<script>
+  import Carousel from './home_components/Carousel.vue';
+  import Accordion from './home_components/Accordion.vue';
+  import tabsCard from './home_components/tabsCard.vue';
+  import About from './home_components/About.vue';
+  import placeHolders from './home_components/placeHolders.vue';
+  import Card from './home_components/Card.vue';
 
-<script>
+
   export default {
-    name: 'HomeIndex'
+    name: 'HomeIndex',
+    components: {
+      Accordion,
+      tabsCard,
+      Carousel,
+      Card,
+      About,
+      placeHolders,
+    }
   }
 </script>
+
+<template>
+  <Carousel/>
+  <Accordion/>
+  <tabsCard/>
+  <About/>
+  <placeHolders/>
+</template>
 
 <style>
   
