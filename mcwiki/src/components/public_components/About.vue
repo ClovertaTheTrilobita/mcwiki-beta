fa32baea2de94dfb7f7fccb618648601b2d4ad88
ClovertaTheTrilobita <cloverta@petalmail.com>
2024-10-13 10:58:38 +0800

    File changes: Changed About.vue Group member Github links added

--- mcwiki/src/components/public_components/About.vue
+++ mcwiki/src/components/public_components/About.vue
@@ -8,9 +8,14 @@
             <a href="#/home">mcwiki.cn</a> &nbsp;|&nbsp;
             <img src="../icons/Minecraft_logo.png" width="15" height="14" alt="icp">&nbsp;
             <a href="https://github.com/ClovertaTheTrilobita/mcwiki-beta" target="_blank" rel="nofollow noreferrer">Github repository</a>&nbsp; 
-            Developed by @Steve's
+            Developed by
           </p>
-          <p class="copyleft">Group 3 &nbsp;|&nbsp; Laur Steve Raydon Hurlburt Tony</p>
+          <p class="copyleft">Group 3 &nbsp;|&nbsp; 
+            <a href="https://github.com/ATwhiteIIIQwQ" class="link-dark link-underline-opacity-25 link-underline-opacity-100-hover">Laur</a>&nbsp;
+            <a href="https://github.com/ClovertaTheTrilobita" class="link-dark link-underline-opacity-25 link-underline-opacity-100-hover">Steve</a>&nbsp;
+            <a href="https://github.com/Theo-polis" class="link-dark link-underline-opacity-25 link-underline-opacity-100-hover">Raydon</a>&nbsp;
+            <a href="https://github.com/zzh-zzh66" class="link-dark link-underline-opacity-25 link-underline-opacity-100-hover">Hurlburt</a>&nbsp;
+            <a href="#" class="link-dark link-underline-opacity-25 link-underline-opacity-100-hover">Tony</a></p>
         </div>
       </div>
     </div>
@@ -19,7 +24,13 @@
 
 <script>
   export default {
-    name: "About"
+    name: "About",
+    data() {
+      return{
+        
+      }
+    },
+    
   }
 </script>
 
