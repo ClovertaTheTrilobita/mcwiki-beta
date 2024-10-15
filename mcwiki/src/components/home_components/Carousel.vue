451d483dfa7dee16298caa8af6ad8adbac325a23
ATwhiteIIIQwQ <atwhite0527@163.com>
2024-10-16 00:20:18 +0800

    updated home page new background

--- mcwiki/src/components/home_components/Carousel.vue
+++ mcwiki/src/components/home_components/Carousel.vue
@@ -1,26 +1,26 @@
 <template>
-  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div id="Carousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#Carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#Carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#Carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="./images/7.jpg" class="d-block w-100" alt="...">
+        <img src="./images/Slide01.jpg" class="d-block w-100 Slide" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="./images/9.jpg" class="d-block w-100" alt="...">
+        <img src="./images/Slide02.jpg" class="d-block w-100 Slide" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="./images/3.jpg" class="d-block w-100" alt="...">
+        <img src="./images/Slide03.jpg" class="d-block w-100 Slide" alt="...">
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#Carousel" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#Carousel" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
@@ -34,6 +34,12 @@
 </script>
 
 <style>
+
+.Slide {
+  max-height: 800px;
+  object-fit: cover;
+}
+
 </style>
 
 <!--
