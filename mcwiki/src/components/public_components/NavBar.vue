e34511a528f9f2e72c7d3bfef6af8193d86596b2
ClovertaTheTrilobita <cloverta@petalmail.com>
2024-10-16 18:24:17 +0800

    Search js framework added

--- mcwiki/src/components/public_components/NavBar.vue
+++ mcwiki/src/components/public_components/NavBar.vue
@@ -62,7 +62,7 @@
             </div>
 
             <form class="d-flex" role="search" >
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" clearable id="myInput">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" clearable id="myInput" ref="myInput">
               <button class="btn btn-outline-light" type="submit" @click="SearchPushed">Search</button>
             </form>
 
@@ -71,16 +71,23 @@
       </nav>
     </div>
   </div>
+  <div style="display: none;">
+    <search v-bind:search="search"></search>
+  </div>
 </template>
 
 <script>
 import Category from '../category_components/Category.vue';
+import SearchList from '../search_components/SearchList.vue';
 
 
 export default {
   name: "NavBar",
   methods: {
     SearchPushed() {
+      this.search = document.getElementById("myInput").value;
+      console.log("Searched: " + this.search)
+      SearchList.methods.searchPushed(this.search)
       window.location.href = '#/search'
     },
     categroyClicked(){
@@ -89,9 +96,13 @@ export default {
   },
   data() {
     return {
-      username: 'Laurentina'
+      username: 'Laurentina',
+      search:[]
     };
-  }
+  },
+  components: {
+    'search': SearchList
+  },
 }
 
 </script>
