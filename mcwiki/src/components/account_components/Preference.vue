b57a145992d1b9a89a6de68dccdb204a77bd88b1
zzh-zzh66 <zihauzhou02@gmail.com>
2024-10-20 17:42:30 +0800

    changed Preference.vue

--- mcwiki/src/components/account_components/Preference.vue
+++ mcwiki/src/components/account_components/Preference.vue
@@ -14,6 +14,16 @@
 
 <script>
 import PreferenceCard from './PreferenceCard.vue';
+// 使用静态import的图片
+import image101 from './media/101.jpg'; 
+import image103 from './media/103.jpg'; 
+import image104 from './media/104.jpg'; 
+import image105 from './media/105.jpg'; 
+import image106 from './media/106.jpg'; 
+import image107 from './media/107.jpg'; 
+import image108 from './media/108.jpg'; 
+import image109 from './media/background_02.png'; 
+
 
 export default {
   components: {
@@ -24,51 +34,51 @@ export default {
       // 模拟的动态数据
       posts: [
         {
+          title: '文章test1',
+          description: '这是关于文章test1的描述内容。',
+          image: image101, // 使用静态import的图片
+          likes: 10,
+        },
+        {
+          title: '文章test2',
+          description: '这是关于文章test2的描述内容。',
+          image: image103,
+          likes: 15,
+        },
+        {
           title: '文章1',
           description: '这是关于文章1的描述内容。',
-          image: './media/a12.jpg',
-          likes: 10,
+          image: image104,
+          likes: 7,
         },
         {
           title: '文章2',
           description: '这是关于文章2的描述内容。',
-          image: 'https://via.placeholder.com/200x300.png?text=文章2',
-          likes: 15,
+          image: image105,
+          likes: 7,
         },
         {
           title: '文章3',
           description: '这是关于文章3的描述内容。',
-          image: 'https://via.placeholder.com/200x300.png?text=文章3',
+          image: image106,
           likes: 7,
         },
         {
           title: '文章4',
-          description: '这是关于文章3的描述内容。',
-          image: 'https://via.placeholder.com/200x300.png?text=文章3',
+          description: '这是关于文章4的描述内容。',
+          image: image107,
           likes: 7,
         },
         {
           title: '文章5',
-          description: '这是关于文章3的描述内容。',
-          image: 'https://via.placeholder.com/200x300.png?text=文章3',
+          description: '这是关于文章5的描述内容。',
+          image: image108,
           likes: 7,
         },
         {
           title: '文章6',
-          description: '这是关于文章3的描述内容。',
-          image: 'https://via.placeholder.com/200x300.png?text=文章3',
-          likes: 7,
-        },
-        {
-          title: '文章6',
-          description: '这是关于文章3的描述内容。',
-          image: 'https://via.placeholder.com/200x300.png?text=文章3',
-          likes: 7,
-        },
-        {
-          title: '文章6',
-          description: '这是关于文章3的描述内容。',
-          image: 'https://via.placeholder.com/200x300.png?text=文章3',
+          description: 'asjdbnaiushdbiuwbdiuawbdfiubnasoiudjnasjud。',
+          image: image109,
           likes: 7,
         },
       ],
