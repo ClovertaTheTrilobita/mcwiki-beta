bb2f20bc4fdb3bec34f1bfe387662df86e6533e5
zzh-zzh66 <zihauzhou02@gmail.com>
2024-10-22 21:08:42 +0800

    Contact.vue  store.js

--- mcwiki/src/components/Index_components/ContactIndex.vue
+++ mcwiki/src/components/Index_components/ContactIndex.vue
@@ -2,7 +2,7 @@
   <div class="d-flex justify-content-center">
     <div class="card" style="width: 80rem; margin-top: 100px;">
       <div class="card-body">
-        <img src="/public/Images/Daily/Luckin.jpg" class="card-img-top Card-img" alt="Image" id="source" style="height: 300px; width:auto">
+        <img src="/Images/Daily/Luckin.jpg" class="card-img-top Card-img" alt="Image" id="source" style="height: 300px; width:auto">
         <h4 class="card-title">Contact us here.</h4>
         <address>
           <strong>Hainan University</strong><br>
